f5c118f268b2a4b2d72490a3bbfe1b3610360170
Daniel Kudinov <danielkudinov17@gmail.com>
2024-04-26 21:25:45 +0300

    add components

---
+++ kino/src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const Basement = styled.footer`
+  background-color: #c25353fa;
+  height: 100px;
+  width: 100vw;
+  position: fixed; 
+  left: 0;
+  bottom: 0; 
+`;
+
+const Footer = () => {
+  return (
+    <>
+      <Basement />
+    </>
+  );
+};
+
+export default Footer;
---
+++ kino/src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+
+const Heading = styled.header`
+    display: flex;
+    justify-content: space-around;
+    background-color: #c25353fa;
+    height: 100px;
+    width: 100vw; 
+    position: absolute; 
+    left: 0; 
+    top: 0; 
+`
+const Header = () => {
+    return ( 
+        <>
+        <Heading>
+            <h1>
+                Kino
+            </h1>
+
+        </Heading>
+        </>
+     );
+}
+ 
+export default Header;
