154ffbfe10eab0806f694e4e90c126003abc23a9
Daniel Kudinov <danielkudinov17@gmail.com>
2024-04-27 16:50:04 +0300

    add pagination

--- kino/src/components/MovieCard/index.tsx
+++ kino/src/components/MovieCard/index.tsx
@@ -31,40 +31,79 @@ const MovieTitle = styled.h3`
   font-size: 18px;
   margin-bottom: 5px;
 `;
+const PaginationWrapper = styled.div`
+    display: flex;
+    margin-top: 20px;
+    padding: 10px 0px 20px 0px;
+`;
 
+const PaginationButton = styled.button<{ active?: boolean }>`
+  background-color: ${({ active }) => (active ? "#c25353fa" : "#f2f2f2")};
+  color: ${({ active }) => (active ? "white" : "black")};
+  border: none;
+  padding: 8px 16px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 0 4px;
+  cursor: pointer;
+`;
 const MovieCard: React.FC = () => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const containerRef = useRef<HTMLDivElement | null>(null);
-
-  const api_key = 'G10HV4T-ATN4EVY-M2A74H4-N9DR5HJ';
-  const api_url = 'https://api.kinopoisk.dev/v1.4/movie?page=1&limit=10&selectFields=id&selectFields=name&selectFields=enName&selectFields=alternativeName&selectFields=names&selectFields=description&selectFields=type&selectFields=typeNumber&selectFields=status&selectFields=rating&selectFields=poster&selectFields=top10&notNullFields=rating.kp&notNullFields=poster.url&sortField=alternativeName&sortType=1&type=movie&status=announced';
-
-  async function getMovies(url: string | URL | Request) {
-    try {
-      const resp = await fetch(url, {
-        headers: {
-          "Content-Type": 'application/json',
-          "X-API-KEY": api_key
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+    const containerRef = useRef<HTMLDivElement | null>(null);
+  
+    const api_key = "G10HV4T-ATN4EVY-M2A74H4-N9DR5HJ";
+    const api_url = (page: number) =>
+      `https://api.kinopoisk.dev/v1.4/movie?page=${page}&limit=4&selectFields=id&selectFields=name&selectFields=enName&selectFields=alternativeName&selectFields=names&selectFields=description&selectFields=type&selectFields=typeNumber&selectFields=status&selectFields=rating&selectFields=poster&selectFields=top10&notNullFields=rating.kp&notNullFields=poster.url&sortField=alternativeName&sortType=1&type=movie&status=announced`;
+  
+    async function getMovies(url: string | URL | Request) {
+      try {
+        const resp = await fetch(url, {
+          headers: {
+            "Content-Type": "application/json",
+            "X-API-KEY": api_key,
+          },
+        });
+  
+        if (!resp.ok) {
+          throw new Error(`Failed to fetch movie data: ${resp.statusText}`);
         }
-      });
-
-      if (!resp.ok) {
-        throw new Error(`Failed to fetch movie data: ${resp.statusText}`);
+  
+        const data = await resp.json();
+        setMovies(data.docs);
+        setTotalPages(Math.ceil(data.total / 4));
+      } catch (error) {
+        console.error("Error fetching movie data:", error);
       }
-
-      const data = await resp.json();
-      setMovies(data.docs);
-    } catch (error) {
-      console.error("Error fetching movie data:", error);
     }
-  }
-
-  useEffect(() => {
-    getMovies(api_url);
-  }, []);
+  
+    useEffect(() => {
+      getMovies(api_url(currentPage));
+    }, [currentPage]);
+  
+    const handlePageChange = (page: number) => {
+      setCurrentPage(page);
+    };
+  
 
   return (
     <div ref={containerRef}>
+        <PaginationWrapper>
+            {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+            (page) => (
+                <PaginationButton
+                key={page}
+                onClick={() => handlePageChange(page)}
+                active={page === currentPage}
+                >
+                {page}
+                </PaginationButton>
+            )
+            )}
+        </PaginationWrapper>  
       <Card>
         {movies.map((movie, index) => (
           <MovieItem key={index}>
