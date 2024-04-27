a398c8be0ce0e2fd718a938aaa6e25621c541d43
Daniel Kudinov <danielkudinov17@gmail.com>
2024-04-27 20:43:59 +0300

    add

--- kino/src/components/Header/index.tsx
+++ kino/src/components/Header/index.tsx
@@ -2,26 +2,24 @@ import styled from "styled-components";
 import SwitchTheme from "../SwitchTheme";
 
 const Heading = styled.header`
-    display: flex;
+display: flex;
     justify-content: space-around;
-    align-items: baseline;
     background-color: #c25353fa;
     height: 100px;
-    width: 100vw; 
-    position: absolute; 
-    left: 0; 
-    top: 0; 
+    width: 100vw;
+    position: absolute;
+    left: 0;
+    top: 0;
+    align-items: center;
 `
 const Header = () => {
     return ( 
-        <>
         <Heading>
             <h1>
                 Kino
             </h1>
                 <SwitchTheme/>
         </Heading>
-        </>
      );
 }
  
